100926889d1e65c4a4c36adb37a3420f6b3411cb
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-24 23:47:10 +0900

    Modify for macOS

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import installExtension, {
   REACT_DEVELOPER_TOOLS,
   REDUX_DEVTOOLS
 } from "electron-devtools-installer";
-
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import url from "url";
 
 let win = null;
+let isQuit = false;
+
+function isMac() {
+  return process.platform === "darwin"; 
+}
 
 function createWindow() {
   win = new BrowserWindow({ width: 1024, height: 960 });
@@ -25,21 +29,47 @@ function createWindow() {
     protocol: "file",
     slashes: true
   }));
+
+  win.on("close", (e) => {
+    if (isQuit) {
+      return;
+    }
+
+    if (!isMac()) {
+      return;
+    }
+
+    e.preventDefault();
+    win.hide();
+  });
+
   win.on("closed", () => {
     win = null;
+  });
+
+  app.on("before-quit", () => {
+    isQuit = true;
+  })
+
+  app.on("activate", () => {
+    if (!isMac()) {
+      return;
+    }
+
+    win.show();
   });
 }
 
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on("active", () => {
-  if (win === null) {
-    createWindow();
+  if (win !== null) {
+    return;
   }
+
+  createWindow();
 });
