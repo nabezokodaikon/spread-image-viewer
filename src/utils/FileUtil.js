308b7f973e53d785cf1e9548e6c1ab4d87855d92
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-26 08:57:27 +0900

    Add FileUtil

---
+++ src/utils/FileUtil.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import fs from "fs";
+import path from "path";
+import { Seq } from "Immutable";
+
+export default {
+  getImageFiles(directoryPath) {
+    return Seq(fs.readdirSync(directoryPath))
+      .filter(f => fs.statSync(path.join(directoryPath, f)).isFile() && /^(?!\.).*(\.jpg|\.gif|\.bmp|\.png)$/.test(f))
+      .sort((a, b) => {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+      })
+      .toArray(); 
+  }
+}
