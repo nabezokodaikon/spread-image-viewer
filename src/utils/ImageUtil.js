55fc5a77b701a3d0db3152f918cfee60c3da5b4c
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-25 23:53:01 +0900

    Modify to default export

--- src/utils/ImageUtil.js
+++ src/utils/ImageUtil.js
@@ -3,12 +3,14 @@
 import fs from "fs";
 import Canvas from "canvas";
 
-export function getImageSize(filePath) {
-  const buffer = fs.readFileSync(filePath);
-  const image = new Canvas.Image();
-  image.src = buffer;
-  return {
-    width: image.width,
-    height: image.height
-  };
+export default {
+  getImageSize: function(filePath) {
+    const buffer = fs.readFileSync(filePath);
+    const image = new Canvas.Image();
+    image.src = buffer;
+    return {
+      width: image.width,
+      height: image.height
+    };
+  }
 }
