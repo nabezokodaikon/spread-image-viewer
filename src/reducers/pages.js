093e02cb0c7f0adf5d12341a05721144f482bd9a
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-26 12:33:04 +0900

    Add open file reducer

--- src/reducers/pages.js
+++ src/reducers/pages.js
@@ -1,11 +1,96 @@
 "use strict";
 
+import path from "path";
+import FileUtil from "../utils/FileUtil";
 import ImageUtil from "../utils/ImageUtil";
+import {
+  OPEN_FILE,
+  REDRAW_PAGE,
+  SINGLE_NEXT_PAGE,
+  SINGLE_PREVIEW_PAGE,
+  DOUBLE_NEXT_PAGE,
+  DOUBLE_PREVIEW_PAGE
+} from "../constants/ActionTypes";
 
-function pages(a) {
-  const ret1 = ImageUtil.getImageSize(a);
-  const ret2 = ImageUtil.getImageSize(ret1);
-  return ret2;
+const initialState = {
+  directory: "",
+  files: [],
+  main: "",
+  sub: ""
+};
+
+function openFile(state, mainFilePath) {
+  const directory = path.dirname(mainFilePath);
+  const files = FileUtil.getImageFiles(directory);
+  if (files.length < 1) {
+    return state;
+  }
+
+  const main = path.basename(mainFilePath);
+  const mainIndex = files.indexOf(main);
+  if (mainIndex < 0) {
+    return state;
+  }
+
+  const mainOnlyState = {
+    directory: directory,
+    files: files,
+    main: main,
+    sub: ""
+  };
+
+  const mainSize = ImageUtil.getImageSize(mainFilePath);
+  if (mainSize.height > mainSize.width) {
+    return mainOnlyState;
+  }
+
+  const subIndex = mainIndex + 1;
+  if (subIndex < files.length) {
+    const sub = files[subIndex];
+    const subFilePath = path.join(directory, sub);
+    const subSize = ImageUtil.getImageSize(subFilePath);
+    if (subSize.height > subSize.width) {
+      return mainOnlyState;
+    } else {
+      return {
+        directory: directory,
+        files: files,
+        main: main,
+        sub: sub
+      };
+    }
+  } else {
+    const sub = files[0];
+    const subFilePath = path.join(directory, sub);
+    const subSize = ImageUtil.getImageSize(subFilePath);
+    if (subSize.height > subSize.width) {
+      return mainOnlyState;
+    } else {
+      return {
+        directory: directory,
+        files: files,
+        main: main,
+        sub: sub
+      };
+    }
+  }
 }
 
-export default pages;
+export default function pages(state = initialState, action) {
+  switch (action.type) {
+    case OPEN_FILE:
+      return openFile(state, action.filePath);
+    case REDRAW_PAGE:
+      return;
+    case SINGLE_NEXT_PAGE:
+      return;
+    case SINGLE_PREVIEW_PAGE:
+      return;
+    case DOUBLE_NEXT_PAGE:
+      return;
+    case DOUBLE_PREVIEW_PAGE:
+      return;
+    default:
+      throw `Action type ${action.type} is not define.`
+  }
+}
--- src/reducers/pages.spec.js
+++ src/reducers/pages.spec.js
@@ -1,15 +1,145 @@
+import * as types from "../constants/ActionTypes";
 import pages from "./pages";
+import FileUtil from "../utils/FileUtil";
 import ImageUtil from "../utils/ImageUtil";
 
 describe("pages reducer", () => {
-  it("example test", () => {
-    // ImageUtil.getImageSize = jest.fn((a) => a * 10);
+  it("should handle OPEN_FILE", () => {
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => []);
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "foo"
+      })
+    ).toEqual({
+      directory: "",
+      files: [],
+      main: "",
+      sub: ""
+    });
+
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => ["img01"]);
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "foo"
+      })
+    ).toEqual({
+      directory: "",
+      files: [],
+      main: "",
+      sub: ""
+    });
+
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => ["img01"]);
     ImageUtil.getImageSize = jest.fn()
-      .mockImplementationOnce(a => a + 1)
-      .mockImplementationOnce(a => a * 3);
-    const ret = pages(2);
-    expect(
-      ret
-    ).toEqual(9);
+      .mockImplementationOnce(f => {
+        return { width: 1, height: 2 };
+      });
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "/img/img01"
+      })
+    ).toEqual({
+      directory: "/img",
+      files: ["img01"],
+      main: "img01",
+      sub: ""
+    });
+
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => ["img01", "img02"]);
+    ImageUtil.getImageSize = jest.fn()
+      .mockImplementationOnce(f => {
+        return { width: 2, height: 2 };
+      })
+      .mockImplementationOnce(f => {
+        return { width: 1, height: 2 };
+      });
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "/img/img01"
+      })
+    ).toEqual({
+      directory: "/img",
+      files: ["img01", "img02"],
+      main: "img01",
+      sub: ""
+    });
+
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => ["img01", "img02"]);
+    ImageUtil.getImageSize = jest.fn()
+      .mockImplementationOnce(f => {
+        return { width: 2, height: 2 };
+      })
+      .mockImplementationOnce(f => {
+        return { width: 2, height: 2 };
+      });
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "/img/img01"
+      })
+    ).toEqual({
+      directory: "/img",
+      files: ["img01", "img02"],
+      main: "img01",
+      sub: "img02"
+    });
+
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => ["img01", "img02", "img03"]);
+    ImageUtil.getImageSize = jest.fn()
+      .mockImplementationOnce(f => {
+        return { width: 2, height: 2 };
+      })
+      .mockImplementationOnce(f => {
+        return { width: 1, height: 2 };
+      });
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "/img/img03"
+      })
+    ).toEqual({
+      directory: "/img",
+      files: ["img01", "img02", "img03"],
+      main: "img03",
+      sub: ""
+    });
+
+    FileUtil.getImageFiles = jest.fn()
+      .mockImplementationOnce(f => ["img01", "img02", "img03"]);
+    ImageUtil.getImageSize = jest.fn()
+      .mockImplementationOnce(f => {
+        return { width: 2, height: 2 };
+      })
+      .mockImplementationOnce(f => {
+        return { width: 2, height: 2 };
+      });
+    expect(pages(
+      undefined,
+      {
+        type: types.OPEN_FILE,
+        filePath: "/img/img03"
+      })
+    ).toEqual({
+      directory: "/img",
+      files: ["img01", "img02", "img03"],
+      main: "img03",
+      sub: "img01"
+    });
   });
 });
