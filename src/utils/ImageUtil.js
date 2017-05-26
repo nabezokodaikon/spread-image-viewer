dbffccab977b5fcb5bea684a0de537bdcac9dba8
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-26 15:47:47 +0900

    Modify ImageUtil

--- src/utils/ImageUtil.js
+++ src/utils/ImageUtil.js
@@ -4,11 +4,12 @@ import fs from "fs";
 import Canvas from "canvas";
 
 export default {
-  getImageSize: function(filePath) {
+  getImageData: function(filePath) {
     const buffer = fs.readFileSync(filePath);
     const image = new Canvas.Image();
     image.src = buffer;
     return {
+      data: buffer, // typeof(buffer) === "object"
       width: image.width,
       height: image.height
     };
