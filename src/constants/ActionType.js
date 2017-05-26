90fe7d6577a8a5aa30a366d725ae0c2cac0f8cd4
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-26 11:01:11 +0900

    Add action type constants

---
+++ src/constants/ActionType.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export const OPEN_FILE = "OPEN_FILE"; 
+export const REDRAW = "REDRAW";
+export const SINGLE_NEXT = "SINGLE_NEXT";
+export const SINGLE_PREVIEW = "SINGLE_PREVIEW";
+export const DOUBLE_NEXT = "DOUBLE_NEXT";
+export const DOUBLE_PREVIEW = "DOUBLE_PREVIEW";
+export const REDRAW = "REDRAW";
