2e8ae5e237a905e11571b6e371138767ab6b7ebe
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-31 20:01:22 +0900

    Fix get image files function
    Upper case string can be used for regex.

--- src/utils/FileUtil.js
+++ src/utils/FileUtil.js
@@ -7,7 +7,7 @@ import { Seq } from "Immutable";
 export default {
   getImageFiles(directoryPath) {
     return Seq(fs.readdirSync(directoryPath))
-      .filter(f => fs.statSync(path.join(directoryPath, f)).isFile() && /^(?!\.).*(\.jpg|\.gif|\.bmp|\.png)$/.test(f))
+      .filter(f => fs.statSync(path.join(directoryPath, f)).isFile() && /^(?!\.).*(\.jpg|\.gif|\.bmp|\.png)$/.test(f.toLowerCase()))
       .sort((a, b) => {
         if (a < b) return -1;
         if (a > b) return 1;
